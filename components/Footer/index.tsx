b2f17c577781eb262fece43b87785468f3778977
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2022-01-16 00:50:36 +0100

    Fixed an error with fotter

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,14 +1,13 @@
 import { FC } from "react";
 import { ReadmeLogo } from "../ReadmeLogo";
 
-import style from "./Footer.module.css";
+import style from "./Footer.module.scss";
 
 export const Footer: FC = () => {
   return (
     <div className={style.footer}>
-      <p>
-        Laget med ❤ av <ReadmeLogo small maxWidth={"7em"} />
-      </p>
+      <p>Laget med ❤ av </p>
+      <ReadmeLogo small maxWidth={"7em"} />
     </div>
   );
 };
