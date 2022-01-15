a2873e4deb80db5c3a6871d72eca1fec38136fba
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2022-01-16 00:30:59 +0100

    Clear out a console log

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,6 @@ export async function getStaticProps() {
 }
 
 const Home: NextPage<{ editionData: IEditionData[] }> = ({ editionData }) => {
-  console.log(editionData);
   return (
     <>
       <Head>
