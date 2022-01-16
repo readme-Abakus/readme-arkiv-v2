9dc252cd2fb86618c08c00e002e5c2c7457b3741
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2022-01-16 01:16:41 +0100

    Fixed a centering issue in the footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -6,8 +6,8 @@ import style from "./Footer.module.scss";
 export const Footer: FC = () => {
   return (
     <div className={style.footer}>
-      <p>Laget med ❤ av </p>
-      <ReadmeLogo small maxWidth={"7em"} />
+      <p>Laget med 📸✍🏻📰 av </p>
+      <ReadmeLogo maxWidth={"7em"} />
     </div>
   );
 };
--- components/ReadmeLogo/index.tsx
+++ components/ReadmeLogo/index.tsx
@@ -3,17 +3,14 @@ import { FC } from "react";
 
 import styles from "./ReadmeLogo.module.css";
 import logoSrc from "../../public/images/readme_hvit.png";
-import logoSrcSmall from "../../public/images/readme_liten_hvit.png";
 import blackLogoSrc from "../../public/images/readme.png";
-import blackLogoSrcSmall from "../../public/images/readme_liten.png";
 
 export const ReadmeLogo: FC<{
   maxWidth?: string;
   maxHeight?: string;
-  small?: boolean;
-}> = ({ maxWidth, maxHeight, small }) => {
+}> = ({ maxWidth, maxHeight }) => {
   return (
-    <div className={styles.readmeLogo}>
+    <div className={styles.readmeLogo} style={{ maxWidth, maxHeight }}>
       <div
         className={styles.logoHvit + " logoHvit"}
         style={{
@@ -21,11 +18,7 @@ export const ReadmeLogo: FC<{
           maxWidth,
         }}
       >
-        <Image
-          src={small ? logoSrcSmall : logoSrc}
-          alt="readmeLogoHvit"
-          layout={"responsive"}
-        />
+        <Image src={logoSrc} alt="readmeLogoHvit" layout={"responsive"} />
       </div>
       <div
         className={styles.logoSort + " logoSort"}
@@ -34,11 +27,7 @@ export const ReadmeLogo: FC<{
           maxWidth,
         }}
       >
-        <Image
-          src={small ? blackLogoSrcSmall : blackLogoSrc}
-          alt="readmeLogoHvit"
-          layout={"responsive"}
-        />
+        <Image src={blackLogoSrc} alt="readmeLogoHvit" layout={"responsive"} />
       </div>
     </div>
   );
