deeba8a6a246fb9956f15d7ab4ae7b9f762733c5
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2022-01-30 17:50:15 +0100

    Rename layout component

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -5,7 +5,7 @@ import { ReadmeLogo } from "../ReadmeLogo";
 
 import styles from "./Layout.module.css";
 
-export default function Header({ children }: { children: ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className={styles.app}>
       <AppNavbar />
