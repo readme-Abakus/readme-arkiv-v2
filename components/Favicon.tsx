48dda22b7c1abc130b06461c82f2e2ee363b8942
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2022-02-05 19:49:14 +0100

    Attempt favicon fix

--- components/Favicon.tsx
+++ components/Favicon.tsx
@@ -4,29 +4,29 @@ export const Favicon = () => {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="favicons/apple-touch-icon.png"
+        href="/favicons/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="favicons/favicon-32x32.png"
+        href="/favicons/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="favicons/favicon-16x16.png"
+        href="/favicons/favicon-16x16.png"
       />
-      <link rel="manifest" href="favicons/site.webmanifest" />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
       <link
         rel="mask-icon"
-        href="favicons/safari-pinned-tab.svg"
+        href="/favicons/safari-pinned-tab.svg"
         color="#5bbad5"
       />
-      <link rel="shortcut icon" href="favicons/favicon.ico" />
+      <link rel="shortcut icon" href="/favicons/favicon.ico" />
       <meta name="msapplication-TileColor" content="#ffc40d" />
-      <meta name="msapplication-config" content="favicons/browserconfig.xml" />
+      <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
       <meta name="theme-color" content="#ffffff" />
     </>
   );
