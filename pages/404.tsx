6d6045e3f014e38a742f39b96bea4cf06e3d145c
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2022-06-11 15:04:55 +0200

    Add title to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/image";
 import { Fade } from "react-bootstrap";
 
@@ -5,12 +6,17 @@ import redaktør from "../public/images/redaktør.png";
 
 export default function Custom404() {
   return (
-    <Fade appear in>
-      <div>
-        <Image src={redaktør} alt="Forvirret redaktør" />
-        <h1>404</h1>
-        <h4>Oups! Vi finner visst ikke den siden.</h4>
-      </div>
-    </Fade>
+    <>
+      <Head>
+        <title>readme - 404</title>
+      </Head>
+      <Fade appear in>
+        <div>
+          <Image src={redaktør} alt="Forvirret redaktør" />
+          <h1>404</h1>
+          <h4>Oups! Vi finner visst ikke den siden.</h4>
+        </div>
+      </Fade>
+    </>
   );
 }
