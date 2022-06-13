811780d431b407c3d8441eceea743546da40766d
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2022-06-13 22:23:52 +0200

    Formatting

--- pages/drikkelek.tsx
+++ pages/drikkelek.tsx
@@ -2,22 +2,20 @@ import Head from "next/head";
 import Image from "next/image";
 import { Fade } from "react-bootstrap";
 
-import drikkelek from "../public/images/drikkelek.png"
+import drikkelek from "../public/images/drikkelek.png";
 
 const Drikkelek = () => {
-
   return (
-  <Fade appear in>
-    <div>
-      <Head>
-        <title>readme - drikkelek</title>
-      </Head>
-  
-          <Image src={drikkelek} alt="drikkelek"/>
+    <Fade appear in>
+      <div>
+        <Head>
+          <title>readme - drikkelek</title>
+        </Head>
 
-    </div>
-  </Fade>
-  )
-}
+        <Image src={drikkelek} alt="drikkelek" />
+      </div>
+    </Fade>
+  );
+};
 
 export default Drikkelek;
