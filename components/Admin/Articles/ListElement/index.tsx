5303bd35628ace83a28d9be306d1e763b2d74d50
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2022-09-20 22:21:29 +0200

    Link to editions from article list too

--- components/Admin/Articles/ListElement/index.tsx
+++ components/Admin/Articles/ListElement/index.tsx
@@ -11,20 +11,24 @@ interface ListElementProps {
   article: IArticle;
 }
 export const ListElement: FC<ListElementProps> = ({ article }) => {
-  const { edition, title, url, id } = article;
+  const { edition, title, id } = article;
   return (
     <div className={styles.elementStyle}>
       <p>
         {edition} | {title}
       </p>
       <div className={styles.end}>
-        <a href={url} target="_blank" rel="noopener noreferrer">
-          <i className={`material-icons md-36 ${styles.open}`}>
-            remove_red_eye
-          </i>
-        </a>
-        <Link href={ROUTES.EDIT_ARTICLE.replace(":id", id)} passHref>
-          <i className={`material-icons md-36 ${styles.edit}`}>edit</i>
+        <Link href={ROUTES.EDITION.replace(":id", edition)}>
+          <a target="_blank" rel="noopener noreferrer">
+            <i className={`material-icons md-36 ${styles.open}`}>
+              remove_red_eye
+            </i>
+          </a>
+        </Link>
+        <Link href={ROUTES.EDIT_ARTICLE.replace(":id", id)}>
+          <a style={{ color: "unset" }}>
+            <i className={`material-icons md-36 ${styles.edit}`}>edit</i>
+          </a>
         </Link>
         <DeleteButton onClick={() => deleteArticle(id)} />
       </div>
