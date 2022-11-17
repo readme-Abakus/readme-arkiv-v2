64770c8195896b4ab5ae2999172bc8fac8887e32
Andreas Hammer Håversen <andreas.haversen@bekk.no>
2022-11-17 23:47:41 +0100

    Fix page on article list

--- components/Admin/Articles/ListElement/index.tsx
+++ components/Admin/Articles/ListElement/index.tsx
@@ -4,7 +4,10 @@ import { DeleteButton } from "../../Common/DeleteButton";
 import { ROUTES } from "../../../../utils/routes";
 
 import styles from "./ListElement.module.css";
-import { deleteArticle } from "../../../../lib/Firebase/firebaseClientAPIs";
+import {
+  deleteArticle,
+  getPageNumber,
+} from "../../../../lib/Firebase/firebaseClientAPIs";
 import { IArticle } from "../../../../lib/types";
 
 interface ListElementProps {
@@ -18,7 +21,12 @@ export const ListElement: FC<ListElementProps> = ({ article }) => {
         {edition} | {title}
       </p>
       <div className={styles.end}>
-        <Link href={ROUTES.EDITION.replace(":id", edition)}>
+        <Link
+          href={
+            ROUTES.EDITION.replace(":id", edition) +
+            `#page=${getPageNumber(article)}`
+          }
+        >
           <a target="_blank" rel="noopener noreferrer">
             <i className={`material-icons md-36 ${styles.open}`}>
               remove_red_eye
