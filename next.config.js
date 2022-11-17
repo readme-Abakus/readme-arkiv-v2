1a94054bc6fd50c85c9404b0347186ba0f4f2eed
Andreas Hammer Håversen <andreas.haversen@bekk.no>
2022-11-17 23:22:57 +0100

    Ditch seperate page for url rewrites

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
+const editionURLPattern = `${
+  process.env.NODE_ENV === "production"
+    ? "https://storage.googleapis.com"
+    : "http://localhost:9199"
+}/readme-arkiv.appspot.com/pdf/:year/:year-:no.pdf`;
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["storage.googleapis.com", "localhost"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/edition/:year-:no",
+        destination: editionURLPattern,
+      },
+    ];
   },
 };
 
