4b483a6c74acde9ed8b155a13d6614acded8679f
magnhan <45832668+magnhan@users.noreply.github.com>
2023-03-25 20:45:15 +0100

    Added "Midtsider" option to navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -11,83 +11,107 @@ import { signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 
 export const AppNavbar: FC = () => {
-  const [user, loading] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
 
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  return (
-    <nav className={styles.navbar}>
-      <LinkButton route={ROUTES.HOME} exactMatch onClick={() => setOpen(false)}>
-        Arkiv
-      </LinkButton>
-      <ul className={open ? `${styles.links} ${styles.active}` : styles.links}>
-        <li>
-          <LinkButton route={ROUTES.SEARCH} onClick={() => setOpen(false)}>
-            Søk
-          </LinkButton>
-        </li>
-        <li>
-          <a href="https://abakus.no/" onClick={() => setOpen(false)}>
-            Abakus.no
-          </a>
-        </li>
-        <li className={styles.lightSwitch}>
-          <LightSwitch />
-        </li>
-        {!loading && user && (
-          <>
-            <li>
-              <LinkButton route={ROUTES.ADMIN} onClick={() => setOpen(false)}>
-                Admin
-              </LinkButton>
-            </li>
-            <li>
-              <a
-                href="#"
-                onClick={() => {
-                  setOpen(false);
-                  signOut(auth);
-                }}
-              >
-                Logg ut
-              </a>
-            </li>
-          </>
-        )}
-      </ul>
-      <span
-        className={`material-icons ${styles.icon} ${open ? styles.active : ""}`}
-        onClick={() => setOpen(!open)}
-      >
-        menu
-      </span>
-    </nav>
-  );
+    return (
+        <nav className={styles.navbar}>
+            <LinkButton
+                route={ROUTES.HOME}
+                exactMatch
+                onClick={() => setOpen(false)}
+            >
+                Arkiv
+            </LinkButton>
+            <ul
+                className={
+                    open ? `${styles.links} ${styles.active}` : styles.links
+                }
+            >
+                <li>
+                    <LinkButton
+                        route={ROUTES.SEARCH}
+                        onClick={() => setOpen(false)}
+                    >
+                        Søk
+                    </LinkButton>
+                </li>
+                <li>
+                    <LinkButton
+                        route={ROUTES.CENTREFOLDS}
+                        onClick={() => setOpen(false)}
+                    >
+                        Midtsider
+                    </LinkButton>
+                </li>
+                <li>
+                    <a href="https://abakus.no/" onClick={() => setOpen(false)}>
+                        Abakus.no
+                    </a>
+                </li>
+                <li className={styles.lightSwitch}>
+                    <LightSwitch />
+                </li>
+                {!loading && user && (
+                    <>
+                        <li>
+                            <LinkButton
+                                route={ROUTES.ADMIN}
+                                onClick={() => setOpen(false)}
+                            >
+                                Admin
+                            </LinkButton>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                onClick={() => {
+                                    setOpen(false);
+                                    signOut(auth);
+                                }}
+                            >
+                                Logg ut
+                            </a>
+                        </li>
+                    </>
+                )}
+            </ul>
+            <span
+                className={`material-icons ${styles.icon} ${
+                    open ? styles.active : ""
+                }`}
+                onClick={() => setOpen(!open)}
+            >
+                menu
+            </span>
+        </nav>
+    );
 };
 
 const LinkButton = ({
-  route,
-  onClick,
-  children,
-  exactMatch = false,
+    route,
+    onClick,
+    children,
+    exactMatch = false,
 }: {
-  route: string;
-  onClick?: () => void;
-  exactMatch?: boolean;
-  children: ReactNode;
+    route: string;
+    onClick?: () => void;
+    exactMatch?: boolean;
+    children: ReactNode;
 }) => {
-  const router = useRouter();
-  let className;
-  if (exactMatch) {
-    className = router.pathname == route ? styles.active : "";
-  } else {
-    className = router.pathname.startsWith(route) ? styles.active : "";
-  }
-  return (
-    <Link href={route} passHref>
-      <a className={className} onClick={onClick}>
-        {children}
-      </a>
-    </Link>
-  );
+    const router = useRouter();
+    let className;
+    if (exactMatch) {
+        className = router.pathname == route ? styles.active : "";
+    } else {
+        className = router.pathname.startsWith(route) ? styles.active : "";
+    }
+    return (
+        <Link href={route} passHref>
+            <a className={className} onClick={onClick}>
+                {children}
+            </a>
+        </Link>
+    );
 };
