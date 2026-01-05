20421dd4a56ed96ac63a99f5bd96787fdbf0c9b2
Viljan Gerbrand Emanuel Van Raaij <vgraaij@stud.ntnu.no>
2026-01-05 09:36:39 +0100

    refactor: CSS cleanup

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,13 +1,10 @@
 import { ReactNode } from "react";
 import { Footer } from "../Footer";
 import { AppNavbar } from "../Navbar";
-import { ReadmeLogo } from "../ReadmeLogo";
-
-import styles from "./Layout.module.css";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className={`text-foreground bg-background ${styles.app}`}>
+    <div className="relative">
       <AppNavbar />
       <div className="pt-[40px] px-[20px] md:px-[40px] pb-[100px] gap-[20px] flex flex-col items-center">
         {children}
